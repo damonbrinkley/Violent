af89c5b12fdcd3563a072e72b7e39b1685ced859
Damon Brinkley <damonbrinkley@gmail.com>
2013-10-02 20:38:46 -0400

    Added nmapScan.py and updated portScan.py

--- CH2/portScan.py
+++ CH2/portScan.py
@@ -1,15 +1,25 @@
 import optparse
 from socket import *
+from threading import *
+
+screenLock = Semaphore(value=1)
 
 def connScan(tgtHost, tgtPort):
     try:
         connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
+        connSkt.send('ViolentPython\r\n')
+        results = connSkt.recv(100)
+        screenLock.acquire()
         print '[+]%d/tcp open'% tgtPort
-        connSkt.close()
+        print '[+] ' + str(results)
     except:
+        screenLock.acquire()
         print '[-]%d/tcp closed'% tgtPort
-
+    finally:
+        screenLock.release()
+        connSkt.close()
+        
 def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
@@ -24,16 +34,21 @@ def portScan(tgtHost, tgtPorts):
         print '\n[+] Scan Results for: ' + tgtIP
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
-        print 'Scanning port ' + tgtPort
-        connScan(tgtHost, int(tgtPort))
-    
-parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
-parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
-(options, args) = parser.parse_args()
-tgtHost = options.tgtHost
-tgtPort = options.tgtPort
-if (tgtHost == None) | (tgtPort == None):
-    print parser.usage
-    exit(0)
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
+
+def main():
+    parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(', ')
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print parser.usage
+        exit(0)
+    portScan(tgtHost, tgtPorts)
+
+if __name__ == '__main__':
+    main()
     
